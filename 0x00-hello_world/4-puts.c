0519e0d048d32d06412ac665477ad83b4c99f29c
mayowa <preciousmayowa16@gmail.com>
2022-04-10 14:01:04 -0700

    AV

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *main-entry point
+ *Description: Print a string followed by a new line 
+ *Return :0
+ */
+int main(void)
+{
+  puts("\" Programming is like bulding a multilingual puzzle");
+  return (0);
+}
