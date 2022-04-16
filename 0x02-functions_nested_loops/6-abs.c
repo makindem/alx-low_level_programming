9d5a4754401873657c3abe55d72f82d326d01d99
mayowa <preciousmayowa16@gmail.com>
2022-04-15 23:27:19 -0700

    6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -4,3 +4,26 @@
 /**
  * _abs - value absolute
  *
+ * @n: The int to print
+ * Return: Always 0.
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (n * (-1));
+	}
+
+	else if (n == 0)
+	{
+		return (0);
+	}
+
+	else
+	{
+		return (n);
+	}
+
+}
