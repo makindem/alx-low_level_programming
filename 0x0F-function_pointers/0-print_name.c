bdf9d2e608da9d18e81873990149624d6be6674e
makindem <101549270+makindem@users.noreply.github.com>
2022-05-24 14:59:20 +0100

    Update 0-print_name.c

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,29 +1,15 @@
 #include "function_pointers.h"
 
-
-
 /**
- *
- *  * print_name - prints a name
- *
- *   * @name: name's main
- *
- *    * @f: void function
- *
- *     */
-
-
+ * print_name - prints a name
+ * @name: name's main
+ * @f: void function
+ */
 
 void print_name(char *name, void (*f)(char *))
-
 {
-
-		if (name && f)
-
-				{
-
-							f(name);
-
-								}
-
+	if (name && f)
+	{
+		f(name);
+	}
 }
